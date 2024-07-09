6513d5a3d707e1c7f239f7972c13271c0114dae7
taufeeq-ahmed <themaestrotaufeeq@gmail.com>
2024-07-09 20:50:08 +0530

    init saas-website-builder

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Fast Files',
+  description: 'The Next Gen file sharing Tool',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <link rel="icon" href="/favicon.png" sizes="any" />
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
